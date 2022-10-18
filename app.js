51aad6f9d5f2b7bdb62fde8c220398c055815c10
--add <=>
2022-10-18 18:33:09 +0530

    resolve home ejs

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
         if (error) {
             throw error
         }
+        console.log(results.rows);
         res.render('home', { data: results.rows });
     })
 })
