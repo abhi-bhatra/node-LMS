4535cf4b3f16b787827fb84d13cc830b173a80ef
--add <=>
2022-10-19 12:13:13 +0530

    knex setup

--- migrations/20221019063545_create_book_table.ts
+++ migrations/20221019063545_create_book_table.ts
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-    const createQuery = `CREATE TABLE <examples>(
+    const createQuery = `CREATE TABLE zeeve(
       id SERIAL PRIMARY KEY NOT NULL,
       message TEXT,
       created_at TIMESTAMP
@@ -8,6 +8,6 @@ exports.up = function(knex, Promise) {
   }
   
   exports.down = function(knex, Promise) {
-    const dropQuery = `DROP TABLE <examples>`
+    const dropQuery = `DROP TABLE zeeve`
     return knex.raw(dropQuery)
   }
