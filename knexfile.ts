4e4c01d272346fd46772c35d689e54dac0656780
--add <=>
2022-10-19 13:29:19 +0530

    knex setup

--- knexfile.ts
+++ knexfile.ts
@@ -1,20 +1,15 @@
-import dotenv from "dotenv";
+require('ts-node/register');
 
-dotenv.config();
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    },
-    migrations: {
-      directory: __dirname + '/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/seeds'
-    }
-  }
+  client: 'pg',
+  connection: 'postgres://abhinav:abhinav@localhost:5432/api',
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: 'migrations'
+  },
+  timezone: 'UTC'
 };
