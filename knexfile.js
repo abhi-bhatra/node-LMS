807dae917d9d55db76c735b3766250d0d580f3c6
--add <=>
2022-10-20 09:59:36 +0530

    knex JS update

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
+module.exports = {
+  staging: {
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
