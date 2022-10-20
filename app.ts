8f34d27e3db2ac46d431cbcf3c1ef8bc05986ecb
--add <=>
2022-10-20 18:04:20 +0530

    add queries for all

--- app.ts
+++ app.ts
@@ -1,19 +1,8 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { addBook, issueBook, returnBook, deleteBook, getBooks } from "./queries";
-dotenv.config();
 const app = express();
 app.set('view engine', 'ejs');
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || "5432")
-});
 
 app.use(bodyParser.json())
 app.use(
@@ -24,17 +13,16 @@ app.use(
 
 app.get("/", async (req: Request, res: Response) => {
   try {
-    const result = await pool.query("SELECT * FROM books");
-    // const books = JSON.stringify(result.rows);
-    res.render('home', { data: result.rows });
+    const books = await getBooks();
+    res.render("home", { data: books.rows });
   } catch (err) {
-    console.log(err);
+    console.error(err.message);
   }
 });
 
-app.get("/add", (req: Request, res: Response) => {
-  res.render('addbook');
-});
+// app.get("/add", (req: Request, res: Response) => {
+//   res.render('addbook');
+// });
 
 // create a route to add book
 app.post("/", async (req: Request, res: Response) => {
@@ -53,51 +41,48 @@ app.post("/", async (req: Request, res: Response) => {
 });
 
 app.post("/issue", (req: Request, res: Response) => {
-  const name = req.body.book;
-
-  pool.query(
-    'UPDATE books SET STATUS = FALSE WHERE BOOK = $1',
-    [name],
-    (err, ) => {
-      if (err) {
-        console.log(err);
-      }
-      // console.log(result);
+    try {
+      const book = {
+        id: parseInt(req.body.id),
+        name:  req.body.book,
+        author: req.body.author,
+        avail: req.body.status
+      };
+      issueBook(book);
       res.redirect("/");
+    } catch (err) {
+      console.log(err);
     }
-  );
 });
 
 app.post("/return", (req: Request, res: Response) => {
-  const name = req.body.book;
-
-  pool.query(
-    'UPDATE books SET STATUS = TRUE WHERE BOOK = $1',
-    [name],
-    (err, ) => {
-      if (err) {
-        console.log(err);
-      }
-      // console.log(result);
+    try {
+      const book = {
+        id: parseInt(req.body.id),
+        name:  req.body.book,
+        author: req.body.author,
+        avail: req.body.status
+      };
+      returnBook(book);
       res.redirect("/");
+    } catch (err) {
+      console.log(err);
     }
-  );
 });
 
 app.post("/delete", (req: Request, res: Response) => {
-  const name = req.body.book;
-
-  pool.query(
-    'DELETE FROM books WHERE BOOK = $1',
-    [name],
-    (err, ) => {
-      if (err) {
-        console.log(err);
-      }
-      // console.log(result);
+    try {
+      const book = {
+        id: parseInt(req.body.id),
+        name:  req.body.book,
+        author: req.body.author,
+        avail: req.body.status
+      };
+      deleteBook(book);
       res.redirect("/");
+    } catch (err) {
+      console.log(err);
     }
-  );
 });
 
 app.listen(process.env.PORT, () => {
