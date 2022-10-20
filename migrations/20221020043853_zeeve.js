68e3746b77d83f7c09d4a76563f756685b42d3ca
--add <=>
2022-10-20 10:10:09 +0530

    knex JS update

---
+++ migrations/20221020043853_zeeve.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.string('role').notNullable();
+        table.timestamps(true, true);
+    }
+    );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    knex.schema.dropTable('users');
+};
