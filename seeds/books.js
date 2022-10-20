bf2f14ec2b6851dfa7d17e0f990ffbf19319eda0
--add <=>
2022-10-20 10:46:41 +0530

    knex JS update

--- seeds/books.js
+++ seeds/books.js
@@ -13,10 +13,4 @@ exports.seed = async function(knex) {
     {id: 5, book: 'Book5', author: 'auth5', status: 'FALSE'},
     {id: 6, book: 'Book6', author: 'auth6', status: 'TRUE'}
   ]);
-  await knex('books').del()
-  await knex('users').insert([
-    {id: 1, name: 'user1', email: 'user1@example.com'},
-    {id: 2, name: 'user2', email: 'user2@example.com'},
-    {id: 3, name: 'user3', email: 'user3@example.com'}
-  ]);
 };
